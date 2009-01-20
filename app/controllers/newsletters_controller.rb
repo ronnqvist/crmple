8929e72862b1dc5421672f98d194866365937ba5
Simon Rönnqvist <simon@iki.fi>
2009-01-20 23:19:39 +0200

    Initial work on newletters

---
+++ app/controllers/newsletters_controller.rb
@@ -0,0 +1,24 @@
+class NewslettersController < ApplicationController
+  def index
+    @roles = Role.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
